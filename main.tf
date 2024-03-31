c3d1132cd7af93f794f7c424ea2195542ce02918
mey21009 <131422456+mey21009@users.noreply.github.com>
2024-03-30 19:12:36 -0600

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,42 @@
+resource "google_compute_address" "static_address" {
+  name   = "ipv4-cicd-test"
+  region = "us-west1"
+}
+resource "google_compute_instance" "web-server" {
+  name         = "cit262-vm-cicd"
+  project      = "lateral-guild-416623"
+  machine_type = "e2-medium"
+  zone         = "us-west1-b"
+  tags         = ["http-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.static_address.address
+    }
+  }
+  metadata_startup_script = <<-SCRIPT
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+wait
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+SCRIPT
+} 
+
+
+
